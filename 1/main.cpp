c2aef2338b68df1ab4fb284b80921c69ad46dce3
Pascal Wey <pascal@pfft.ch>
2021-12-01 22:33:32 +0100

    Solution for day 1!

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include "input_1.h"
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+int main(int, char **)
+{
+    std::vector<int> differences;
+    std::adjacent_difference(std::begin(depths), std::end(depths), std::back_inserter(differences));
+    std::cout << std::count_if(std::next(std::begin(differences)), std::end(differences), [](int a)
+                               { return a > 0; })
+              << std::endl;
+
+    std::vector<int> sums;
+    for (auto iter = std::begin(depths); iter < std::end(depths) - 2; iter++)
+    {
+        sums.emplace_back(*iter + *(iter + 1) + *(iter + 2));
+    }
+
+    differences.clear();
+    std::adjacent_difference(std::begin(sums), std::end(sums), std::back_inserter(differences));
+    std::cout << std::count_if(std::next(std::begin(differences)), std::end(differences), [](int a)
+                               { return a > 0; })
+              << std::endl;
 }
