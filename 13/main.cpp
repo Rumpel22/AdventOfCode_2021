73d52442b45d4c260f7450e45c72d0e5c24b8ea9
Pascal Wey <pascal@pfft.ch>
2021-12-13 18:55:15 +0100

    Much faster solution for day 13

--- 13/main.cpp
+++ 13/main.cpp
@@ -3,7 +3,35 @@
 #include <algorithm>
 #include <iostream>
 
-using Map = std::vector<std::vector<bool>>;
+struct Map
+{
+    const size_t width;
+    const size_t height;
+
+    Map(size_t x, size_t y) : width(x), height(y), map((y + 1) * (x + 1), 0) {}
+
+    int &get(size_t x, size_t y)
+    {
+        return map.at(width * y + x);
+    }
+
+    size_t countDots() const
+    {
+        return std::count_if(std::begin(map), std::end(map), [](const int x)
+                             { return x != 0; });
+    }
+
+    Map &operator=(const Map &rhs)
+    {
+        const_cast<size_t &>(height) = rhs.height;
+        const_cast<size_t &>(width) = rhs.width;
+        map = rhs.map;
+        return *this;
+    }
+
+private:
+    std::vector<int> map;
+};
 
 Map parseMap(const Coordinates &coordinates)
 {
@@ -14,30 +42,14 @@ Map parseMap(const Coordinates &coordinates)
                                       { return a.second < b.second; }))
                        .second;
 
-    Map map = std::vector(max_y + 1, std::vector(max_x + 1, false));
+    Map map = Map(max_x, max_y);
 
     for (auto [x, y] : coordinates)
     {
-        map[y][x] = true;
+        map.get(x, y) = true;
     }
 
     return map;
-}
-
-size_t countDots(const Map &map)
-{
-    size_t count = 0;
-    for (const auto &row : map)
-    {
-        for (const auto &field : row)
-        {
-            if (field)
-            {
-                ++count;
-            }
-        }
-    }
-    return count;
 }
 
 int main(int, char **)
@@ -46,19 +58,21 @@ int main(int, char **)
 
     for (auto fold : folds)
     {
-        const size_t old_x = map[0].size();
-        const size_t old_y = map.size();
+        const size_t old_x = map.width;
+        const size_t old_y = map.height;
 
         const size_t new_x = fold.first == 'x' ? fold.second : old_x;
         const size_t new_y = fold.first == 'y' ? fold.second : old_y;
 
-        Map newMap = std::vector(new_y, std::vector(new_x, false));
+        Map newMap(new_x, new_y);
         for (size_t y = 0; y < old_y; ++y)
         {
             if (fold.first == 'y' && y == new_y)
             {
                 continue;
             }
+
+            size_t set_y = (y < new_y ? y : new_y - (y - new_y));
             for (size_t x = 0; x < old_x; ++x)
             {
                 if (fold.first == 'x' && x == new_x)
@@ -66,20 +80,19 @@ int main(int, char **)
                     continue;
                 }
                 size_t set_x = (x < new_x ? x : new_x - (x - new_x));
-                size_t set_y = (y < new_y ? y : new_y - (y - new_y));
-                newMap[set_y][set_x] = (newMap[set_y][set_x] | map[y][x]);
+                newMap.get(set_x, set_y) |= map.get(x, y);
             }
         }
         map = newMap;
 
-        std::cout << "There are " << countDots(map) << " dots visible" << std::endl;
+        std::cout << "There are " << map.countDots() << " dots visible" << std::endl;
     }
 
-    for (size_t y = 0; y < map.size(); ++y)
+    for (size_t y = 0; y < map.height; ++y)
     {
-        for (size_t x = 0; x < map[0].size(); ++x)
+        for (size_t x = 0; x < map.width; ++x)
         {
-            std::cout << (map[y][x] ? '*' : ' ');
+            std::cout << (map.get(x, y) ? '*' : ' ');
         }
         std::cout << std::endl;
     }
