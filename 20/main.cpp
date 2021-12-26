f6af73ca9a37c105ad04a9f834fcab0dc5545e32
Pascal Wey <pascal@pfft.ch>
2021-12-26 14:07:28 +0100

    Refactored solution day 20

--- 20/main.cpp
+++ 20/main.cpp
@@ -1,71 +1,108 @@
 #include "input_20.h"
 
 #include <algorithm>
+#include <array>
 #include <iostream>
-#include <iterator>
-#include <numeric>
 
-std::vector<bool> parseString(const std::string &string)
+struct Image
 {
-    std::vector<bool> bits;
-    std::transform(std::begin(string), std::end(string), std::back_insert_iterator(bits), [](char c)
+    Image(size_t height, size_t width, bool unknown) : height(height),
+                                                       width(width),
+                                                       data(height * width, false),
+                                                       unknownPixel(unknown)
+    {
+    }
+
+    Image(const std::vector<std::string> &image, bool unknown) : height(image.size()),
+                                                                 width(image[0].size()),
+                                                                 unknownPixel(unknown)
+    {
+        data.reserve(height * width);
+        for (const auto &row : image)
+        {
+            for (const auto &c : row)
+            {
+                data.push_back(c == '#');
+            }
+        }
+    }
+
+    bool get(size_t x, size_t y) const
+    {
+        if (x < 0 || x >= width)
+        {
+            return unknownPixel;
+        }
+        if (y < 0 || y >= height)
+        {
+            return unknownPixel;
+        }
+        return data[y * width + x];
+    }
+
+    void set(size_t x, size_t y, bool value)
+    {
+        data[y * width + x] = value;
+    }
+
+    size_t lightPixels() const
+    {
+        return std::count_if(std::begin(data), std::end(data), [](bool b)
+                             { return b; });
+    }
+    size_t height;
+    size_t width;
+    bool unknownPixel;
+
+private:
+    std::vector<bool> data;
+};
+
+using Algorithm = std::array<bool, 512>;
+
+template <size_t size>
+std::array<bool, size> parseString(const std::string &string)
+{
+    std::array<bool, size> bits;
+    std::transform(std::begin(string), std::end(string), std::begin(bits), [](char c)
                    { return c == '#'; });
     return bits;
 }
 
-bool getPixel(const std::vector<std::vector<bool>> &image, int row, int col, bool unknownPixel)
+Image applyAlgorithm(const Image &image, const Algorithm &algorithm)
 {
-    if (row < 0 || row >= image.size())
+    Image newImage(image.height + 2, image.width + 2, algorithm[0] && !image.unknownPixel);
+    for (int row = -1; row <= static_cast<int>(image.height); ++row)
     {
-        return unknownPixel;
-    }
-    if (col < 0 || col >= image[row].size())
-    {
-        return unknownPixel;
-    }
-    return image[row][col];
-}
-
-std::vector<std::vector<bool>> applyAlgorithm(const std::vector<std::vector<bool>> &image, const std::vector<bool> &algorithm, bool unknownPixel)
-{
-    std::vector<std::vector<bool>> newImage;
-    for (int row = -1; row <= static_cast<int>(image.size()); ++row)
-    {
-        std::vector<bool> newRow;
-        for (int col = -1; col <= static_cast<int>(image[0].size()); ++col)
+        for (int col = -1; col <= static_cast<int>(image.width); ++col)
         {
             unsigned int number = 0;
-            number = (number << 1) + getPixel(image, row - 1, col - 1, unknownPixel);
-            number = (number << 1) + getPixel(image, row - 1, col, unknownPixel);
-            number = (number << 1) + getPixel(image, row - 1, col + 1, unknownPixel);
-            number = (number << 1) + getPixel(image, row, col - 1, unknownPixel);
-            number = (number << 1) + getPixel(image, row, col, unknownPixel);
-            number = (number << 1) + getPixel(image, row, col + 1, unknownPixel);
-            number = (number << 1) + getPixel(image, row + 1, col - 1, unknownPixel);
-            number = (number << 1) + getPixel(image, row + 1, col, unknownPixel);
-            number = (number << 1) + getPixel(image, row + 1, col + 1, unknownPixel);
+            number = (number << 1) + image.get(col - 1, row - 1);
+            number = (number << 1) + image.get(col, row - 1);
+            number = (number << 1) + image.get(col + 1, row - 1);
+            number = (number << 1) + image.get(col - 1, row);
+            number = (number << 1) + image.get(col, row);
+            number = (number << 1) + image.get(col + 1, row);
+            number = (number << 1) + image.get(col - 1, row + 1);
+            number = (number << 1) + image.get(col, row + 1);
+            number = (number << 1) + image.get(col + 1, row + 1);
 
-            newRow.push_back(algorithm[number]);
+            newImage.set(col + 1, row + 1, algorithm[number]);
         }
-        newImage.push_back(newRow);
     }
     return newImage;
 }
 
 int main(int, char **)
 {
-    std::vector<bool> bitEnhancement = parseString(enhancement);
-    std::vector<std::vector<bool>> bitImage;
-    std::transform(std::begin(image), std::end(image), std::back_insert_iterator(bitImage), [](const std::string &row)
-                   { return parseString(row); });
+    const auto bitEnhancement = parseString<512>(enhancement);
+    Image bitImage(image, false);
 
     for (int i = 0; i < 50; ++i)
     {
-        bitImage = applyAlgorithm(bitImage, bitEnhancement, i % 2 != 0);
+        bitImage = applyAlgorithm(bitImage, bitEnhancement);
     }
 
-    size_t lightPixels = std::accumulate(std::begin(bitImage), std::end(bitImage), size_t(0), [](size_t sum, const std::vector<bool> &row)
-                                         { return sum + std::count_if(std::begin(row), std::end(row), [](bool b)
-                                                                      { return b; }); });
+    size_t lightPixels = bitImage.lightPixels();
     std::cout << "There are " << lightPixels << " pixels lit in the image." << std::endl;
 }
